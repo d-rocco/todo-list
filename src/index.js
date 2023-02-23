113d9d0ea0ae97d1d8e37626e414a73ad7bc5e66
d-rocco <dhrocco27@gmail.com>
2023-02-23 15:27:23 -0500

    Functionality of adding a task to selected project
    Also made basic HTML for task form, it will look prettier once it's styled. To make sure the task added goes with the right project, I added get/set for the most recently selected project to add it to.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import projectManager from "./projectManager";
 import "./styles/main.css";
 import { format, formatDistance, subDays } from "date-fns";
 
+/***Project Related Code***/
 function submitProject() {
   const textInput = document.getElementById("add-project-input");
   // makes sure there are no duplicated names
@@ -21,6 +22,7 @@ function submitProject() {
       format(new Date(), "MMM dd, yyyy")
     );
     projectManager.projects.push(addedProject);
+    projectManager.setRecentProject(addedProject);
     textInput.value = "";
     // testing
     for (let i = 0; i < projectManager.projects.length; i++)
@@ -57,4 +59,66 @@ document
       e.preventDefault();
       submitProject();
     }
+  });
+
+/***Task Related Code***/
+
+function submitTask() {
+  const formData = document.querySelectorAll(".add-task-form");
+  const existingTaskNames = projectManager
+    .getRecentProject()
+    .tasks.map((task) => `${task.getTitle().toLowerCase()}`);
+  if (
+    formData[0][0].value !== "" &&
+    !existingTaskNames.includes(formData[0][0].value.toLowerCase())
+  ) {
+    if (formData[0][2].value !== "") {
+      const year = formData[0][2].valueAsDate.getFullYear();
+      const month = formData[0][2].valueAsDate.getMonth();
+      const day = formData[0][2].valueAsDate.getUTCDate();
+      document.querySelector(".add-task-btn").hidden = false;
+      document.querySelector(".add-task-form").hidden = true;
+      const addedTask = task(
+        formData[0][0].value,
+        formData[0][1].value,
+        format(new Date(year, month, day), "MMM dd, yyyy"),
+        formData[0][3].value
+      );
+      projectManager.getRecentProject().tasks.push(addedTask);
+      for (let i = 0; i < 3; i++) {
+        formData[0][i].value = "";
+      }
+    } // if due date is not filled out
+    else {
+      document.querySelector(".add-task-btn").hidden = false;
+      document.querySelector(".add-task-form").hidden = true;
+      const addedTask = task(
+        formData[0][0].value,
+        formData[0][1].value,
+        "No Due Date",
+        formData[0][3].value
+      );
+      projectManager.getRecentProject().tasks.push(addedTask);
+      for (let i = 0; i < 3; i++) {
+        formData[0][i].value = "";
+      }
+    }
+  } else {
+    document.querySelector(".add-task-btn").hidden = false;
+    document.querySelector(".add-task-form").hidden = true;
+  }
+}
+
+// provides form for inputting new task
+document.querySelector(".add-task-btn").addEventListener("click", function (e) {
+  document.querySelector(".add-task-btn").hidden = true;
+  document.querySelector(".add-task-form").hidden = false;
+});
+
+// creates new task when user clicks '+' button
+document
+  .querySelector(".submit-new-task-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    submitTask();
   });
--- src/projectManager.js
+++ src/projectManager.js
@@ -2,11 +2,17 @@
 Project Manager:
     Array of projects
 */
+import project from "./project";
 
 const projectManager = (() => {
   let projects = [];
+  let recentProject = project("", "");
+  const getRecentProject = () => recentProject;
+  const setRecentProject = (newProject) => (recentProject = newProject);
   return {
     projects,
+    getRecentProject,
+    setRecentProject,
   };
 })();
 
